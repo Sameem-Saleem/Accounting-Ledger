aedfbf78ecb70968485711869577dc0ed3082a2a
Sameem <sameem@berkeley.edu>
2024-10-15 14:29:31 -0700

    User can view all entries in CSV

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,27 @@
 package com.pluralsight;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
+    private static final String dataFileName = "transactions.csv";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (Transaction n : pullData()) {
+            System.out.println(n);
+        }
+    }
+
+    private static ArrayList<Transaction> pullData() {
+        ArrayList<Transaction> data = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(dataFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.add(new Transaction(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
     }
 }
---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+
+    /** The date which the transaction was entered. */
+    public final LocalDate date;
+    /** The time which the transaction was entered. */
+    public final LocalTime time;
+    /** The description of the transaction. */
+    public final String description;
+    /** The vendor associates with this transaction. */
+    public final String vendor;
+    /** The amount of the transaction. Negative indicates a payment, positive indicates a deposit. */
+    public final float amount;
+    /** The string representation of a transaction. In the format:
+     * <p>
+     * {@code YYYY-MM-DD|HH:MM:SS|DESCRIPTION|VENDOR|AMOUNT}
+     */
+    private final String information;
+
+    /**
+     * @param information
+     * This constructor takes in a string formatted like the following:
+     * <p>
+     * {@code YYYY-MM-DD|HH:MM:SS|DESCRIPTION|VENDOR|AMOUNT}
+     * <p>
+     * and assigns the following values to the instance variables, from
+     * left to right - date, time, description, vendor, and amount.
+     */
+    public Transaction(String information) {
+        this.information = information;
+        String[] values = information.split("\\|");
+        this.date = LocalDate.parse(values[0]);
+        this.time = LocalTime.parse(values[1]);
+        this.description = values[2];
+        this.vendor = values[3];
+        this.amount = Float.parseFloat(values[4]);
+    }
+
+    /** Returns the string representation of the {@code Transaction} object.
+     * @return {@code YYYY-MM-DD|HH:MM:SS|DESCRIPTION|VENDOR|AMOUNT}
+     */
+    @Override
+    public String toString() {
+//        return date + "|" + time + "|" + description + "|" +
+        return this.information;
+    }
+}
